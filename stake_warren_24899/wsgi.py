b727601a984558c8799c28ede35f2f465d35f7a8
Crowdbotics <team@crowdbotics.com>
2021-03-07 17:47:30 +0000

    Update models

--- stake_warren_24899/wsgi.py
+++ stake_warren_24899/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stake_warren_24899.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stake_warren_24899.settings")
 
 application = get_wsgi_application()
